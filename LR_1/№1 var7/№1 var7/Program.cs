f5d0850ce03d1e760ad3a03a1cbe956216d2f17b
EgorBerezkin <berezkin07egor@gmail.com>
2025-09-25 08:59:39 +0300

    определяет максимальное и минимальное значение

--- LR_1/№1 var7/№1 var7/Program.cs
+++ LR_1/№1 var7/№1 var7/Program.cs
@@ -20,6 +20,16 @@ namespace _1_var7
             {
                 array[i]= Convert.ToInt32(Console.ReadLine());
             }
+            int max = array[0];
+            for (int i = 1; i < n; i++) 
+            {
+                if (array[i] > max) max = array[i];
+            }
+            int min = array[0];
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i] < min) min = array[i];
+            }
         }
     }
 }
