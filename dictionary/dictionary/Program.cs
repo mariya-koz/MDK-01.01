efac855fe523a16250dafe0accf4e33876940f08
Daniil5903 <st50@WS50-07>
2025-10-25 12:35:27 +0300

    заполнен словарь и организован вывод ключей

--- dictionary/dictionary/Program.cs
+++ dictionary/dictionary/Program.cs
@@ -11,7 +11,14 @@ namespace dictionary
         static void Main(string[] args)
         {
             Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
-
+            string slip = "спать";
+                dict.Add(slip, new List<string>() { "кроватка", "одеялко", "подушка" });
+                string eat = "кушать";
+                dict.Add(eat, new List<string>() { "шавуха", "бутерброд", "чаёк" });
+                string holidays = "выходные";
+                dict.Add(holidays, new List<string>() { "спать", "кушать", "отдыхать" });
+            Console.WriteLine(string.Join(", ", dict.Keys));
+            
         }
     }
 }
