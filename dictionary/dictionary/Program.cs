d6bf9edb050a9833730c8423b075db997a3c1af4
Daniil5903 <st50@WS50-07>
2025-10-25 11:59:03 +0300

    создан пустой словарь

---
+++ dictionary/dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+        }
+    }
+}
