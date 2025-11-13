f4e5c09bd8775ea26a4425a05397e8c1a9604c44
st403 <st403@WS403-07>
2025-11-13 12:16:08 +0300

    создана структура дл хранения информации о продажах телефонов

---
+++ distant_13_11/distant_13_11/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distant_13_11
+{
+    internal class Sale
+    {
+        public struct SalePhone
+        {
+            public DateTime Date;
+            public string Phone;
+            public int Count;
+            public decimal Price;
+        }
+    }
+}
