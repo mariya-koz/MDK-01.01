ce6d8ed709db0e6d69a5d9b5be1dd91f4541446d
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-22 10:31:06 +0300

    создан класс автомобилей. создано 2 объекта, сделан их вывод в виде функции и список со всеми автомобилями

---
+++ NFS_Console/NFS_Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFS_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car();
+            c1.SetModel("Lada");
+            c1.SetSpeed(150);
+            Console.WriteLine(c1.GetModel());
+
+            Car c2 = new Car();
+            c2.SetModel("Mercedes");
+            c2.SetSpeed(300);
+
+            WriteModel(c1);
+            WriteModel(c2);
+
+            List<Car> cars = new List<Car>();
+            cars.Add(c1);
+            cars.Add(c2);
+        }
+
+        static void WriteModel(Car mod)
+        {
+            Console.WriteLine(mod.GetModel());
+        }
+    }
+}
---
+++ NFS_Console/NFS_Console/car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFS_Console
+{
+    class Car
+    {
+        private string model;
+        private int speed;
+
+        public void SetModel(string model_)
+        {
+            model_ = model;
+        }
+
+        public string GetModel()
+        {
+            return model_;
+        }
+
+        public void SetSpeed (int speed_)
+        {
+            speed_ = speed;
+        }
+    }
+}
